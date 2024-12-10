90b0cdf8fc107a08555f11b4158eb854f4d95921
Hugo Jacquet <hugo.jacquet@kheops.ch>
2024-12-10 16:29:15 +0100

    feat(dependencies): Change the folder mounting at boot

--- custom_init.sh
+++ custom_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Mount the host environment shared folder in QEMU
-mount -t 9p -o trans=virtio hostshare /mnt/hostshare
-mkdir -p /mnt/hostshare
+#mount -t 9p -o trans=virtio hostshare /mnt/hostshare
+#mkdir -p /mnt/hostshare
 
 # Execute custom init script
 if [[ -f /mnt/hostshare/init.sh ]];
